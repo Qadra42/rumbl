a894056753b707c8b43a868e1bb0a072f1b11253
Francisco Cuadra <francisco@Francisco-MacBook-Air.local>
2023-01-10 11:38:03 -0300

    finish ch7

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -6,6 +6,7 @@ defmodule Rumbl.Multimedia do
   alias Rumbl.Repo
   alias Rumbl.Multimedia.Video
   alias Rumbl.Accounts # puesto por mi
+  alias Rumbl.Multimedia.Category
   def list_videos do
     Repo.all(Video)
   end
@@ -45,5 +46,15 @@ defmodule Rumbl.Multimedia do
 
   defp user_videos_query(query, %Accounts.User{id: user_id}) do
     from(v in query, where: v.user_id == ^user_id)
+  end
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
+  end
+
+  def list_alphabetical_categories do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
   end
 end
